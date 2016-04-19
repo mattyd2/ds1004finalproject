f3a9c2a9fa0618f594c320598b49771c2198e021
Matthew Dunn <mattyd2@gmail.com>
2016-04-19 19:14:59 -0400

    haversine distance measure added and working summary stats printing properly

--- map_reduce_scripts/bike/reduce.py
+++ map_reduce_scripts/bike/reduce.py
@@ -2,6 +2,7 @@
 
 import sys
 import math
+from haversine import haversine
 
 
 def amountcalc(ridertype, duration):
@@ -9,12 +10,12 @@ def amountcalc(ridertype, duration):
     mins = float(duration)/60.
     # print "minutes calculation", mins
     if ridertype == 'Subscriber':
-        if mins <= 45:
+        if mins < 45:
             return 0.0
-        elif mins > 45 and mins < 75:
+        elif mins >= 45 and mins < 75:
             # print "Subscriber AND if mins > 45 and mins < 75:", 2.50
             return 2.50
-        elif mins > 75 and mins < 105:
+        elif mins >= 75 and mins < 105:
             # print 'Subscriber AND if mins > 75 and mins < 105:', 11.50
             return 11.50
         elif mins >= 105:
@@ -23,13 +24,13 @@ def amountcalc(ridertype, duration):
             mincharges = mins/30
             # print mincharges*9.
             return mincharges*9.
-    if ridertype == 'Customer':
-        if mins <= 30:
+    elif ridertype == 'Customer':
+        if mins < 30:
             return 0.0
-        elif mins > 30 and mins < 60:
+        elif mins >= 30 and mins < 60:
             # print "customer AND mins > 30 and mins < 60", 4.
             return 4.
-        elif mins > 60 and mins < 90:
+        elif mins >= 60 and mins < 90:
             # print "customer AND mins > 60 and mins < 90:", 13.
             return 13.
         elif mins >= 90:
@@ -40,32 +41,68 @@ def amountcalc(ridertype, duration):
             return mincharges*12.
 
 
-def manhattandist():
-    pass
+def manhattandist(start_lat, start_lon, end_lat, end_lon):
+    '''this function is used to calculate the approximate manhattan distance
+    between two sets of lat and lon points. it was built referencing 
+
+    http://stackoverflow.com/questions/32923363/manhattan-distance-for-two-geolocations
+    http://www.movable-type.co.uk/scripts/latlong.html
+
+    input: two sets of lat and long points
+
+    returns: approximate manhattan distnace'''
+
+    start_station_location = (start_lat, start_lon)
+    end_station_location = (end_lat, end_lon)
+    return haversine(start_station_location, end_station_location, miles=True)
 
 currentKey = None
-totaltrips = 0
-amount = 0
+distance = None
+totaltrips = 0.0
+amount = 0.0
+duration = 0.0
+
+# add max and min values to confirm quality
 
 for line in sys.stdin:
     key, values = line.strip().split('\t')
-    print '+_+_+_+_+_+', key
+    # print '+_+_+_+_+_+', key
     valueList = values.split(',')
-    duration = valueList[8]
+    duration = float(valueList[8])
     ridertype = valueList[9]
 
     if key == currentKey:
-        totaltrips += 1
+        totaltrips += 1.0
+        duration += duration
         amount += amountcalc(ridertype, duration)
-        print "amount += amountcalc(ridertype, duration)", amount
+        # print "amount += amountcalc(ridertype, duration)", amount
     else:
         # if the currentKey is NOT None then evals to True
-        if currentKey:
-            avg_amount = amount/totaltrips
-            print "%s\t%s,%s" %(currentKey, totaltrips, amount) 
+        if currentKey is not None:
+            if distance is None:
+                start_lat = float(valueList[2])
+                start_lon = float(valueList[3])
+                end_lat = float(valueList[6])
+                end_lon = float(valueList[7])
+                distance = "{0:.2f}".format(manhattandist(start_lat, start_lon, end_lat,
+                                         end_lon))
+            avg_amount = "{0:.2f}".format(amount/totaltrips)
+            if duration > 0:
+                avg_duration = "{0:.2f}".format(duration/totaltrips)
+            if totaltrips > 5:
+                print "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (currentKey, avg_duration, duration, distance,
+                                                      avg_amount, amount, totaltrips)
         currentKey = key
-        totaltrips = 1
+        totaltrips = 1.0
+        duration = float(valueList[8])
         amount = amountcalc(ridertype, duration)
+        distance = None
 
-avg_amount = amount/totaltrips
-print "%s\t%s,%s" %(currentKey, totaltrips, amount) 
+start_lat = float(valueList[2])
+start_lon = float(valueList[3])
+end_lat = float(valueList[6])
+end_lon = float(valueList[7])
+distance = manhattandist(start_lat, start_lon, end_lat, end_lon)
+print "final entry"
+print "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (currentKey, avg_duration, duration, distance,
+                                      avg_amount, amount, totaltrips)
