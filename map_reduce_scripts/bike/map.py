f1a0ae096a62c406476c5b99bfff66df6fc7417f
Matthew Dunn <mattyd2@gmail.com>
2016-04-19 16:37:20 -0400

    mapper done average cost calculated via reducer done

--- map_reduce_scripts/bike/map.py
+++ map_reduce_scripts/bike/map.py
@@ -4,6 +4,6 @@ import sys
 
 for line in sys.stdin:
     l = line.strip().split(',')
-    key = l[3]+','+l[7]
-    value = l[4]+','+l[1]+','+l[5]+','+l[6]+','+l[8]+','+l[2]+','+l[9]+','+l[10]+','+l[0]+',,'
+    key = l[3]+l[7]
+    value = l[3]+','+l[1]+','+l[5]+','+l[6]+','+l[7]+','+l[2]+','+l[9]+','+l[10]+','+l[0]+','+l[12]
     print "%s\t%s" % (key, value)
--- map_reduce_scripts/bike/reduce.py
+++ map_reduce_scripts/bike/reduce.py
@@ -3,50 +3,69 @@
 import sys
 import math
 
-currentKey = None
-totaltrips = 0
-days = []
-
 
 def amountcalc(ridertype, duration):
-    mins = duration/60
+    # print "duration value", duration
+    mins = float(duration)/60.
+    # print "minutes calculation", mins
     if ridertype == 'Subscriber':
-        if mins > 45 and mins < 75:
+        if mins <= 45:
+            return 0.0
+        elif mins > 45 and mins < 75:
+            # print "Subscriber AND if mins > 45 and mins < 75:", 2.50
             return 2.50
-        if mins > 75 and mins < 105:
-            return 2.50
-        else:
+        elif mins > 75 and mins < 105:
+            # print 'Subscriber AND if mins > 75 and mins < 105:', 11.50
+            return 11.50
+        elif mins >= 105:
+            # print 'Subscriber AND over 105'
             mins = mins-105
             mincharges = mins/30
-            return mincharges*9
+            # print mincharges*9.
+            return mincharges*9.
     if ridertype == 'Customer':
-        if mins > 30 and mins < 60:
-            return 4
-        if mins > 60 and mins < 90:
-            return 9
-        else:
+        if mins <= 30:
+            return 0.0
+        elif mins > 30 and mins < 60:
+            # print "customer AND mins > 30 and mins < 60", 4.
+            return 4.
+        elif mins > 60 and mins < 90:
+            # print "customer AND mins > 60 and mins < 90:", 13.
+            return 13.
+        elif mins >= 90:
+            # print "customer ADN over 90 min"
             mins = mins-90
             mincharges = math.ceil(mins/30)
-            return mincharges*12
+            # print "mincharges*12.", mincharges*12.
+            return mincharges*12.
+
+
+def manhattandist():
+    pass
+
+currentKey = None
+totaltrips = 0
+amount = 0
 
 for line in sys.stdin:
-    key, values = line.strip().split(',')
+    key, values = line.strip().split('\t')
+    print '+_+_+_+_+_+', key
+    valueList = values.split(',')
+    duration = valueList[8]
+    ridertype = valueList[9]
+
     if key == currentKey:
         totaltrips += 1
-        days = list(set(days + [value]))
-
+        amount += amountcalc(ridertype, duration)
+        print "amount += amountcalc(ridertype, duration)", amount
     else:
+        # if the currentKey is NOT None then evals to True
         if currentKey:
-            numDays = len(set(days))
-            aveTrips = "{0:.2f}".format(totaltrips/float(numDays))
-            print "%s\t%s,%s" %(currentKey, totaltrips, aveTrips)       
-        currentKey =  key
+            avg_amount = amount/totaltrips
+            print "%s\t%s,%s" %(currentKey, totaltrips, amount) 
+        currentKey = key
         totaltrips = 1
-        days = [value]
-                
+        amount = amountcalc(ridertype, duration)
 
-aveTrips = "{0:.2f}".format(totaltrips/float(numDays))
-print "%s\t%s,%s" %(currentKey, totaltrips, aveTrips)       
-
-
-print "%s\t%s,%s" % (currentKey, value, value2)
+avg_amount = amount/totaltrips
+print "%s\t%s,%s" %(currentKey, totaltrips, amount) 
