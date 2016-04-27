90273b3a5cdeb51d20bf73c4aa3613fd4585c53a
Matthew Dunn <mattyd2@gmail.com>
2016-04-27 17:45:35 -0400

    built map and reduce along with spanning tree algorithm

--- generalized_map_reduce/map.py
+++ generalized_map_reduce/map.py
@@ -10,7 +10,7 @@ from shapely.geometry import shape, Point
 import math
 
 # load GeoJSON file containing sectors
-with open('/Users/matthewdunn/Dropbox/NYU/Spring2016/BigData/GroupProject/nyccensustracts.json', 'r') as f:
+with open('../../nyccensustracts.json', 'r') as f:
     js = json.load(f)
 
 
@@ -50,18 +50,6 @@ def amountcalc(ridertype, duration):
             return mincharges*12.
 
 
-def geocoder(lat, lon):
-    point = Point(lon, lat)
-
-    # check each polygon to see if it contains the point
-    for feature in js['features']:
-        geometry = feature.get('geometry')
-        polygon = shape(geometry)
-        if polygon.contains(point):
-                properties = feature.get('properties')
-                BoroCT2010 = properties.get('BoroCT2010')
-                return BoroCT2010
-
 for line in sys.stdin:
     l = line.strip().split(',')
 
@@ -71,43 +59,10 @@ for line in sys.stdin:
 
             # calculate amount
             total_amount = amountcalc(l[12], float(l[0]))
-
-            start_census_tract = geocoder(float(l[5]), float(l[6]))
-            end_census_tract = geocoder(float(l[9]), float(l[10]))
-            key = 'bike'+str(start_census_tract)+str(end_census_tract)
-            #         0        1        2        3        4        5        6        7               8               9                10                         11
-            value = l[3]+','+l[1]+','+l[5]+','+l[6]+','+l[7]+','+l[2]+','+l[9]+','+l[10]+','+str(total_amount)+','+l[0]+','+str(start_census_tract)+','+str(end_census_tract)
-            print "%s\t%s" % (key, value)
-
-    # Taxi Mapper
-    elif len(l) == 18:
-        if l[0] != 'vendor_id':
-
-            # calc duration
-            start_datetime = datetime.strptime(l[1], "%m/%d/%y %H:%M")
-            end_datetime = datetime.strptime(l[2], "%m/%d/%y %H:%M")
-            duration = end_datetime-start_datetime
-            duration = int(duration.total_seconds())
-
-            start_census_tract = geocoder(float(l[6]), float(l[5]))
-            end_census_tract = geocoder(float(l[10]), float(l[9]))
-            key = 'taxi'+str(start_census_tract)+str(end_census_tract)
-            #           0           1        2        3          4            5        6         7        8            9                      10                         11
-            value = l[5]+l[6]+','+l[1]+','+l[6]+','+l[5]+','+l[9]+l[10]+','+l[2]+','+l[10]+','+l[9]+','+l[17]+','+str(duration)+','+str(start_census_tract)+','+str(end_census_tract)+','+l[4]
-            print "%s\t%s" % (key, value)
-
-    # Bus Mapper
-    elif len(l) == 11:
-        if l[0] != 'Unnamed: 0':
-            day_string = l[1]
-            if 'Weekday' in day_string:
-                date = '4/4/16'
-            elif 'Sunday' in day_string or 'Saturday' in day_string:
-                date = '4/3/16'
-
-            start_census_tract = geocoder(float(l[6]), float(l[5]))
-            end_census_tract = geocoder(float(l[10]), float(l[9]))
-            key = 'bus'+str(start_census_tract)+str(end_census_tract)
-            #           0           1        2        3          4            5        6         7        8            9                      10                         11
-            value = l[5]+l[6]+','+l[1]+','+l[6]+','+l[5]+','+l[9]+l[10]+','+l[2]+','+l[10]+','+l[9]+','+l[17]+','+str(duration)+','+str(start_census_tract)+','+str(end_census_tract)+','+l[4]
+            start_station_id = l[3]
+            end_station_id = l[7]
+            duration = l[0]
+            key = 'bike'+str(start_station_id)+str(end_station_id)
+            #               0                   1               2
+            value = start_station_id+','+end_station_id+','+duration
             print "%s\t%s" % (key, value)
--- generalized_map_reduce/reduce.py
+++ generalized_map_reduce/reduce.py
@@ -42,62 +42,50 @@ for line in sys.stdin:
     if key == currentKey:
         totaltrips += 1.0
         # print 'BEFORE amount += float(valueList[8])', amount
-        amount += float(valueList[8])
+        # amount += float(valueList[8])
         # print 'AFTER amount += float(valueList[8])', amount
         # print 'BEFORE duration += float(valueList[9])', duration
-        duration += float(valueList[9])
+        duration += float(valueList[2])
         # print 'AFTER duration += float(valueList[9])', duration
-        if currentKey[0:4] != 'bike':
-            distance += float(valueList[12])
+        # if currentKey[0:4] != 'bike':
+        #     distance += float(valueList[12])
 
     else:
         # check if currentKey is set
         if currentKey is not None:
-            if currentKey[0:4] == 'bike':
-                start_lat = float(valueList[2])
-                start_lon = float(valueList[3])
-                end_lat = float(valueList[6])
-                end_lon = float(valueList[7])
-                avg_distance = "{0:.2f}".format(manhattandist(start_lat,
-                                                              start_lon,
-                                                              end_lat,
-                                                              end_lon))
-            else:
-                avg_distance = distance/totaltrips
-            avg_amount = "{0:.2f}".format(amount/totaltrips)
-            avg_duration = "{0:.2f}".format(duration/totaltrips)
-            print "%s,%s,%s,%s,%s,%s,%s,%s" % (currentKey,
-                                               ",".join(valueList[2:4]),
-                                               ",".join(valueList[6:8]),
-                                               avg_amount,
-                                               avg_duration,
-                                               ",".join(valueList[10:12]),
-                                               avg_distance,
-                                               totaltrips)
+            # if currentKey[0:4] == 'bike':
+            #     start_lat = float(valueList[2])
+            #     start_lon = float(valueList[3])
+            #     end_lat = float(valueList[6])
+            #     end_lon = float(valueList[7])
+            #     avg_distance = "{0:.2f}".format(manhattandist(start_lat,
+            #                                                   start_lon,
+            #                                                   end_lat,
+            #                                                   end_lon))
+            # else:
+                # avg_distance = distance/totaltrips
+            # avg_amount = "{0:.2f}".format(amount/totaltrips)
+            start_location_id = valueList[0]
+            end_location_id = valueList[1]
+            avg_duration = int(duration/totaltrips)
+            print "%s,%s,%s" % (end_location_id, avg_duration, start_location_id)
 
         # set the flow control variables equal to values of current entry
         currentKey = key
         totaltrips = 1.0
-        duration = float(valueList[9])
-        amount = float(valueList[8])
-        if currentKey[0:4] != 'bike':
-            distance = float(valueList[12])
+        duration = float(valueList[2])
+        # amount = float(valueList[8])
+        # if currentKey[0:4] != 'bike':
+        #     distance = float(valueList[12])
 
-if currentKey[0:4] == 'bike':
-    start_lat = float(valueList[2])
-    start_lon = float(valueList[3])
-    end_lat = float(valueList[6])
-    end_lon = float(valueList[7])
-    avg_distance = "{0:.2f}".format(manhattandist(start_lat,
-                                                  start_lon,
-                                                  end_lat, end_lon))
-else:
-    avg_distance = distance/totaltrips
-print "%s,%s,%s,%s,%s,%s,%s,%s" % (currentKey,
-                                   ",".join(valueList[2:4]),
-                                   ",".join(valueList[6:8]),
-                                   avg_amount,
-                                   avg_duration,
-                                   ",".join(valueList[10:12]),
-                                   avg_distance,
-                                   totaltrips)
+# if currentKey[0:4] == 'bike':
+#     start_lat = float(valueList[2])
+#     start_lon = float(valueList[3])
+#     end_lat = float(valueList[6])
+#     end_lon = float(valueList[7])
+#     avg_distance = "{0:.2f}".format(manhattandist(start_lat,
+#                                                   start_lon,
+#                                                   end_lat, end_lon))
+# else:
+#     avg_distance = distance/totaltrips
+print "%s,%s,%s" % (end_location_id, avg_duration, start_location_id)
